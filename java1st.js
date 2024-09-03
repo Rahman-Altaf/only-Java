b5310848c85a7dff4838d37f70dd2cc83e593334
Rahman-Altaf <rahmanch.311@gmail.com>
2024-09-04 00:32:48 +0500

    (IF STATEMENT/ IF ELSE STATEMENT)

--- java1st.js
+++ java1st.js
@@ -38,66 +38,107 @@
 // console.log(student["name"]);
 // // student();
 
-let a = 10;
-let b = 20;
-console.log("The Value Of A = ", a, "The Value Of B is = ", b);
-console.log("Addition Of A & B Is = ", a + b);
-console.log("Subtraction Of A & B Is = ", a - b);
-console.log("Multiplication Of A & B Is = ", a * b);
-console.log("Division Of A & B Is = ", a / b);
-console.log("Modulus Of A & B Is = ", a % b);
-console.log("Exponentiation Of A & B Is = ", a ** b);
-// Uniary Operator Increment And Decrement ( Post And Pre Increment And Decrement)
-// Pre Inncrement change Value First than Print See Example
-console.log(
-  "The Actual Value Of A Is 10 but according to Pre Increment The Value Will be 11 ",
-  ++a
-);
-// as it is Decrement
-console.log(
-  "The Actual Value Of A Is 10 but according to Pre Decrement The Value Will be 9 ",
-  --a
-);
-//  As it is Post Increment ( The Value Will Change After Implimenting For Example it print first the actual
-// value and then do increment or decrement )
-console.log(
-  "The Actual Value of B Is 20 And it print 20 because it is post increment So The Answer Will Be 20 ",
-  b++
-);
-// As It Is Decrement
-console.log(
-  "The Actual Value of B Is 20 And it print 20 because it is post Decrement So the Answer Will be 20 ",
-  b--
-);
+// let a = 10;
+// let b = 20;
+// console.log("The Value Of A = ", a, "The Value Of B is = ", b);
+// console.log("Addition Of A & B Is = ", a + b);
+// console.log("Subtraction Of A & B Is = ", a - b);
+// console.log("Multiplication Of A & B Is = ", a * b);
+// console.log("Division Of A & B Is = ", a / b);
+// console.log("Modulus Of A & B Is = ", a % b);
+// console.log("Exponentiation Of A & B Is = ", a ** b);
+// // Uniary Operator Increment And Decrement ( Post And Pre Increment And Decrement)
+// // Pre Inncrement change Value First than Print See Example
+// console.log(
+//   "The Actual Value Of A Is 10 but according to Pre Increment The Value Will be 11 ",
+//   ++a
+// );
+// // as it is Decrement
+// console.log(
+//   "The Actual Value Of A Is 10 but according to Pre Decrement The Value Will be 9 ",
+//   --a
+// );
+// //  As it is Post Increment ( The Value Will Change After Implimenting For Example it print first the actual
+// // value and then do increment or decrement )
+// console.log(
+//   "The Actual Value of B Is 20 And it print 20 because it is post increment So The Answer Will Be 20 ",
+//   b++
+// );
+// // As It Is Decrement
+// console.log(
+//   "The Actual Value of B Is 20 And it print 20 because it is post Decrement So the Answer Will be 20 ",
+//   b--
+// );
 
-// Assignment Operator
-a += 4;
-console.log("Now The Value Will be 10 but the answer will 14 ", a);
-a -= 4;
-console.log("Now The Value Will be 10 but the answer will 6 ", a);
+// // Assignment Operator
+// a += 4;
+// console.log("Now The Value Will be 10 but the answer will 14 ", a);
+// a -= 4;
+// console.log("Now The Value Will be 10 but the answer will 6 ", a);
 
-//  Comparison Operator
-//  Equal To ==
-//  Not Equal To !=
-//  Equal To Operator Example
-console.log("IS a Is Equal To b", a == b);
-console.log("Is a Is Not Equal To B ", a != b);
+// //  Comparison Operator
+// //  Equal To ==
+// //  Not Equal To !=
+// //  Equal To Operator Example
+// console.log("IS a Is Equal To b", a == b);
+// console.log("Is a Is Not Equal To B ", a != b);
 let w = 9;
 let z = 10;
-console.log("Is W Is Equal To Z ", w == z);
-// >, <, >=, <=
-console.log("Is 10 > 20", a > b);
-console.log("Is 10 < 20", a < b);
-console.log("Is 10 >= 20", a >= b);
-console.log("Is 10 <= 20", a <= b);
+// console.log("Is W Is Equal To Z ", w == z);
+// // >, <, >=, <=
+// console.log("Is 10 > 20", a > b);
+// console.log("Is 10 < 20", a < b);
+// console.log("Is 10 >= 20", a >= b);
+// console.log("Is 10 <= 20", a <= b);
 
 // Logical Operators
 // Logical And (&&) both true hona zaroori heyn ( 1 b false to sari false )
-// Logical Not (!) 
-// Logical Or (||) 1 ka true hona zaroori hay
+// Logical Not (!) Koi B Expression agr true dey raha ho ga to usko False bana dey ga or agr false dey raha ho to true kar dey ga
+// Logical Or (||) 2 conditions mey say koi 1 b true hoa to true
 
-// ----------------------------
-// Example Of// Logical And (&&) both true hona zaroori heyn
-let cond1 = w == z;
-let cond2 = w < z;
-console.log("Cond 1 && Cond 2 ", cond1 && cond2);
+// // ----------------------------
+// // Example Of// Logical And (&&) both true hona zaroori heyn
+// let cond1 = w == z;
+// let cond2 = w < z;
+// console.log("Cond 1 && Cond 2 ", cond1 && cond2);
+
+// // Example Of Logical Or// Logical Or (||) 2 conditions mey say koi 1 b true hoa to true
+
+// console.log("Cond 1 || Cond 2 ", cond1 || cond2);
+
+// // Example of Logical Not (!) Koi B Expression agr true dey raha ho ga to usko False bana dey ga or agr false dey raha ho to true kar dey ga
+// console.log(
+//   "9 > 10 Actual Answer Is False But due to NOt operator it show true ",
+//   !(w > z)
+// );
+// console.log(
+//   "9 < 10 Actual Answer Is True But due to NOt operator it show False",
+//   !(w < z)
+// );
+
+// --------------------------------------------------------------------------------------------------------------
+// Conditional Statement
+// If Statement
+// If Else Statement
+// --------------------------------------------------------------------------------------------------------------
+
+// If Statement
+if (w > z) {
+  console.log(z, "Z Is Greater Than w");
+}
+
+// Example Of // If Else Statement  2 Best Example
+// Vote Example
+let age = 13;
+if (age >= 18) {
+  console.log("You Can Vote");
+} else {
+  console.log("You Can Not Vote Because You Are Under Age And Your Age Is ",age);
+}
+//  Next Example Of IS STATEMENT IS ODD AND EVEN NUMBER 
+let num = 26
+if (num % 2 == 0) {
+  console.log(num, "This Number Is Even");
+} else {
+  console.log(num,"This Number Is Odd")
+}
--- practice1.js
+++ practice1.js
@@ -1,13 +1,13 @@
-// Save All the necessary data of PRODUCT display at amazon page
-const product = {
-  title: "Shaver Machine",
-  price: 250,
-  isDeal: true,
-  discount: "35 Percent ",
-  rating: "5 Star",
-};
-console.log(product);
+// // Save All the necessary data of PRODUCT display at amazon page
+// const product = {
+//   title: "Shaver Machine",
+//   price: 250,
+//   isDeal: true,
+//   discount: "35 Percent ",
+//   rating: "5 Star",
+// };
+// console.log(product);
 
-console.log(product.title)
+// console.log(product.title)
 
-// product();
+// // product();
